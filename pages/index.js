21d837b8534a4cea77704ff0dd411df7b8404298
Anthony Galvin <a@anthonygalvin.com>
2022-02-11 23:54:23 +0000

    initial release dashboard

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+
+function sinceLastRelease(releaseDate){
+
+  //Get 1 day in milliseconds
+  const one_day=1000*60*60*24;
+
+  const currentTime = new Date();
+  const elapsedMS = currentTime - Date.parse(releaseDate);
+  let elapsedDays = Math.round(elapsedMS/one_day);
+  return elapsedDays;
+}
+
+export default function Home({ repositories }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +27,75 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          TTG Release Dashboard
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {repositories.map((repository) => (
+          repository.latestRelease && 
+          <>
+            <div className={ sinceLastRelease(repository.latestRelease.createdAt) < 8 ? styles.green + " " + styles.card : styles.red + " " + styles.card}>
+              <h3>{repository.name}</h3>
+              <p>{ sinceLastRelease(repository.latestRelease.createdAt) + ' days since last release' }
+              </p>
+              <p>
+                {repository.latestRelease.name}
+              </p>
+            </div>
+          </>
+        ))}
+      </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+    query{
+      organization(login: "TodayTix"){
+        repositories(first:100, orderBy: { field: NAME, direction: ASC}){
+          nodes{
+            id
+            name
+            latestRelease{
+              id
+              name
+              tagName
+              createdAt
+              author {
+                id
+                name
+                email
+              }
+            }
+            releases(last:10, orderBy: { field: CREATED_AT, direction: DESC}){
+              edges{
+                node{
+                  id
+                  name
+                  tagName
+                  createdAt
+                  author {
+                    id
+                    name
+                    email
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    `,
+  });
+  console.log(data.organization.repositories.nodes);
+  return {
+    props: {
+      repositories: data.organization.repositories.nodes
+    },
+ };
 }
